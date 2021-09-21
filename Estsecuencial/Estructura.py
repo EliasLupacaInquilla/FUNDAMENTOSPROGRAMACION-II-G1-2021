55844d103cd25907664777f17bddbdb2d2f78828
EliasLupacaInqu <>
2021-09-21 23:35:05 +0000

    Elias

---
+++ Estsecuencial/Estructura.py
@@ -0,0 +1,7 @@
+print("Hallar el area de un rombo")
+DM=int(input("Ingrese diagonal mayor:"))
+dm=int(input("Ingrese diagonal menor:"))
+#proceso
+area=(DM*dm)/2
+#datos de salida
+print("el area del rombo es:",area , "mt2") 
