bc4d034ceb0081d6899854dd2d26891aeff7da0e
EliasLupacaInqu <>
2021-10-11 14:54:25 +0000

    Elias

--- EstCiclicas/EstCiclicas.java
+++ EstCiclicas/EstCiclicas.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class EstCiclicas{
   static Scanner sc = new Scanner(System.in);
 
-static void suma20mumerosPares(){
+static void suma20mumerosParesWhile(){
   //Declarar variables e inicializacion de valores
   int numInit=1, sumaNumPar=0;
   //Proceso
@@ -16,11 +16,36 @@ static void suma20mumerosPares(){
   //Datos de Salida
     System.out.println("La suma de numeros pares entre 1 y 20 es:"+sumaNumPar);
 }
-  public static void main(String[] args) {
-    System.out.println("Holas Est. Repetitivas");
-  }
-  
+
+static void suma20numerosParesDowhile(){
+  //Declaracion de variables e inicializacion de variables
+  int sumaNumPar=0, numInit=1;
+  //Proceso
+  do{
+    if(numInit%2==0){
+      sumaNumPar=sumaNumPar+numInit;
+    }
+    numInit++;
+  }while(numInit<=20);
+  //Datos de salida
+  System.out.println("La suma de numeros pares entre 1 y 20 es:"+sumaNumPar);
 }
+
+static void suma20mumerosParesFor(){
+  //Declarar variables e inicializacion de valores
+  int sumaNumPar=0;
+  //Proceso
+    for(int numInit=1; numInit <= 20; numInit++) {
+
+      if(numInit%2==0){
+        sumaNumPar=sumaNumPar+numInit;
+      }
+      System.out.println("numInit:"+numInit+ " sumaNumPar:"+sumaNumPar);      
+    }
+  //Datos de Salida
+    System.out.println("La suma de numeros pares entre 1 y 20 es:"+sumaNumPar);
+}
+
   static void menuOpciones(){ 
     int opcionesA=0;
     System.out.println("-------------------------------");
@@ -31,6 +56,7 @@ static void suma20mumerosPares(){
         switch(opcionesA) {
           case 1: suma20mumerosParesWhile();break;
           case 2: suma20mumerosParesFor();break;
+          case 3: suma20numerosParesDowhile(); break;
           default:
             System.out.println("El Algoritmo no existe!");
         }   
@@ -39,4 +65,10 @@ static void suma20mumerosPares(){
       opcionesA=sc.nextInt();        
     }
   }
+
+  public static void main(String[] args) {
+    System.out.println("Holas Est. Repetitivas");
+    menuOpciones();
+  }
   
+}
